753a2d788b85c2aeee66d5d6aa662f2b3c8e45be
Satyanarayana Kadali <satyakadali@Satyanarayanas-MacBook-Pro.local>
2021-01-24 19:24:49 -0500

    first commit

---
+++ src/test/java/com/satya/NetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
